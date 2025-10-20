4808050d660be4e86fb541de1940c63f6adc38d3
DzaneQ <lucker1996@gmail.com>
2025-10-20 15:46:59 +0200

    feat: Added resolutions to the settings

--- Assets/Scripts/Menu/SettingsMenu.cs
+++ Assets/Scripts/Menu/SettingsMenu.cs
@@ -1,4 +1,7 @@
 using Berty.Settings;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,6 +10,37 @@ namespace Berty.Menu
     public class SettingsMenu : MonoBehaviour
     {
         [SerializeField] private Slider volumeSlider;
+        [SerializeField] private TMP_Dropdown resolutionDropdown;
+        private List<Resolution> resolutionOptions;
+
+        public void Start()
+        {
+            InitializeResolutionOptions();
+        }
+
+        private void InitializeResolutionOptions()
+        {
+            Resolution[] resolutions = Screen.resolutions;
+            List<string> resolutionStrings = new();
+            resolutionOptions = new();
+            int currentResolutionIndex = 0;
+            foreach (Resolution res in resolutions)
+            {
+                string resolutionString = res.width.ToString() + " x " + res.height.ToString();
+                resolutionStrings.Add(resolutionString);
+                resolutionOptions.Add(res);
+                if (Screen.currentResolution.width == res.width && Screen.currentResolution.height == res.height) currentResolutionIndex = resolutionStrings.Count - 1;
+
+            }
+            resolutionDropdown.AddOptions(resolutionStrings);
+            resolutionDropdown.value = currentResolutionIndex;
+        }
+
+        public void UpdateResolution()
+        {
+            int index = resolutionDropdown.value;
+            Screen.SetResolution(resolutionOptions[index].width, resolutionOptions[index].height, true);
+        }
 
         public void UpdateVolume()
         {
